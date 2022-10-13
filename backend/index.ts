b6c92135e09bed14abdfd4096ccd162e415ddb03
daniilmikulik <mikulik086420@gmail.com>
2022-10-13 23:00:57 +0300

    fix(backend): linter errors

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { Express } from "express/ts4.0";
+import swaggerUi from "swagger-ui-express";
 import DatabaseManager from "./database/DatabaseManager";
 import ApiRouter from "./routes/ApiRouter";
 import AdminRouter from "./routes/AdminRouter";
 import errorHandler from "./middlewares/ErrorHandler/ErrorHandler";
-import swaggerUi from "swagger-ui-express";
 
 const port: number | string = process.env.PORT || 8000;
 const app: Express = express();
