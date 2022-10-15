04076ff411813a5e085fb787a5520edde7997678
MolodzeffDanila <russgun1@mail.ru>
2022-10-15 19:53:10 +0300

    fix(project): fix route swagger

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -2,9 +2,9 @@ import { Response, Request } from "express";
 import { Get, Route } from "tsoa";
 import BaseController from "./BaseController";
 
-@Route("api/user/ping")
+@Route("api/user")
 export default class UserController extends BaseController {
-    @Get("/")
+    @Get("/ping")
     public async ping(): Promise<string> {
         return "ok";
     }
