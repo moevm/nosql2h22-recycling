13da58d59052d5d4ddba174f14b81e43b50a8cc6
daniilmikulik <mikulik086420@gmail.com>
2022-11-20 16:08:55 +0300

    feat(client-admin): initialized bootstrap project

---
+++ client-admin/src/App/App.helpers.ts
@@ -0,0 +1,16 @@
+import {Page} from "./App.types";
+
+export const pages: Page[] = [
+    {
+        link: "#home",
+        title: "Home",
+    },
+    {
+        link: "#info",
+        title: "Info",
+    },
+    {
+        link: "#about",
+        title: "About",
+    }
+];
---
+++ client-admin/src/App/index.ts
@@ -0,0 +1,4 @@
+export * from "./App";
+export * from "./App.hooks";
+export * from "./App.helpers";
+export * from "./App.types";
