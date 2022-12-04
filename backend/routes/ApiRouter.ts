afb6fc29de04e49ffd1c8332176c18f1901a0415
jendos2001 <jendos2001@yandex.ru>
2022-12-04 18:58:39 +0300

    feat(backend): add ManagerReceptionController

--- backend/routes/ApiRouter.ts
+++ backend/routes/ApiRouter.ts
@@ -3,6 +3,7 @@ import MainStorageController from "../controllers/MainStorageController";
 import AuthorizationController from "../controllers/AuthorizationController";
 import ReceptionsController from "../controllers/ReceptionsController";
 import TransitController from "../controllers/TransitController";
+import ManagerReceptionController from "../controllers/ManagerReceptionController";
 
 const ApiRouter: Router = express.Router();
 
@@ -10,5 +11,6 @@ ApiRouter.post("/admin/main", MainStorageController.handleAction("main"));
 ApiRouter.post("/login", AuthorizationController.handleAction("login"));
 ApiRouter.post("/admin/receptions", ReceptionsController.handleAction("receptions"));
 ApiRouter.post("/admin/transit", TransitController.handleAction("transit"));
+ApiRouter.post("/manager/reception", ManagerReceptionController.handleAction("reception"));
 
 export default ApiRouter;
