1a95920d766a94439f5d7949c347da12c4093602
daniilmikulik <mikulik086420@gmail.com>
2022-12-04 17:01:11 +0300

    feat(client): added registration

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth/next";
 import YandexProvider from "next-auth/providers/yandex";
+import { server } from "../../../config/axios";
+
+type IUser = {
+    name: string;
+    email: string;
+    image: string;
+    id: string;
+};
+
+const buildRequestBody = (user: IUser) => {
+    const [firstName, lastName] = user.name.split(" ");
+    const [login] = user.email.split("@");
+    return {
+        firstName,
+        lastName,
+        email: user.email,
+        login,
+        id: user.id,
+    };
+};
 
 export default NextAuth({
     providers: [
@@ -9,4 +29,14 @@ export default NextAuth({
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET || "",
+    callbacks: {
+        async signIn({ user }) {
+            try {
+                await server.post("/register", buildRequestBody(user as IUser));
+            } catch (err) {
+                if (err instanceof Error) console.error(err.message);
+            }
+            return true;
+        },
+    },
 });
