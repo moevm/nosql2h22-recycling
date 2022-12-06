8aa2af85c03a367d89da46d775ebbaecda1e44ee
daniilmikulik <mikulik086420@gmail.com>
2022-12-07 02:58:31 +0300

    feat(client): add filter

--- client/components/pages/StatisticsPage/StatisticsPage.tsx
+++ client/components/pages/StatisticsPage/StatisticsPage.tsx
@@ -9,6 +9,7 @@ import { Hero, IHeroProps } from "../../shared/Hero";
 import styles from "./StatisticsPage.module.css";
 import { TableData } from "../../shared/TableData";
 import { server } from "../../../config/axios";
+import { useFilters } from "./StatisticsPage.hooks";
 
 export type IStatisticsPageProps = IHeroProps & {};
 
@@ -41,90 +42,6 @@ export const columns = [
         text: "Status",
     },
 ];
-
-export const data: any[] = [
-    {
-        id: <a href="/order/1?amount=1&wasteType=Metal&price=50"> 1 </a>,
-        date: "13-10-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "34kg",
-        status: "Status",
-    },
-    {
-        id: <a href="/order/45"> 45 </a>,
-        date: "13-11-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "32kg",
-        status: "Status",
-    },
-    {
-        id: <a href="/order/111"> 111 </a>,
-        date: "12-10-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "14kg",
-        status: "Status",
-    },
-    {
-        id: <a href="/order/124"> 124 </a>,
-        date: "13-10-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "354kg",
-        status: "Status",
-    },
-    {
-        id: 22,
-        date: "13-10-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "324kg",
-        status: "Status",
-    },
-    {
-        id: 158,
-        date: "03-10-2020",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "31kg",
-        status: "Status",
-    },
-    {
-        id: 11,
-        date: "13-11-2021",
-        type_of_waste: "Plastic",
-        subtype: "Aluminium",
-        among: "324kg",
-        status: "Status",
-    },
-    {
-        id: 12,
-        date: "03-14-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "31kg",
-        status: "Status",
-    },
-    {
-        id: 13,
-        date: "19-10-2021",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "324kg",
-        status: "Status",
-    },
-    {
-        id: 3736,
-        date: "12-10-2020",
-        type_of_waste: "Metal",
-        subtype: "Aluminium",
-        among: "31kg",
-        status: "Status",
-    },
-];
-
 // @ts-ignore
 export const buildData = (res) => {
     console.log(res);
@@ -147,6 +64,8 @@ export const buildData = (res) => {
     });
 };
 
+const filters = ["All", "Metal", "Plastic", "Glass", "Paper", "Organic", "Battery"];
+
 export const StatisticsPage = ({ title, description, footer }: IStatisticsPageProps) => {
     const { data: session, status } = useSession();
     const [page, setPage] = useState<number>(1);
@@ -154,6 +73,7 @@ export const StatisticsPage = ({ title, description, footer }: IStatisticsPagePr
     const [total, setTotal] = useState<number>(5);
     const [orders, setOrders] = useState([]);
     const [loyalty, setLoyalty] = useState(0);
+    const [filter, setFilter] = useState<string>("All");
 
     useEffect(() => {
         const login = session?.user?.email?.split("@")[0];
@@ -168,13 +88,15 @@ export const StatisticsPage = ({ title, description, footer }: IStatisticsPagePr
         const login = session?.user?.email?.split("@")[0];
         console.log(login);
         // @ts-ignore
-        server.get(`/user/orders?login=${login}&page=${page}&perPage=${perPage}`).then((res) => {
+        server.get(filter !== "All"
+            ? `/user/orders?login=${login}&page=${page}&perPage=${perPage}&filter=${filter}`
+            : `/user/orders?login=${login}&page=${page}&perPage=${perPage}&filter=.`).then((res) => {
             setTotal(res.data.count);
             setOrders(buildData(res.data.result));
         }).catch((e) => {
             console.error(e);
         });
-    }, [page, perPage, status]);
+    }, [page, perPage, status, filter]);
 
     return (
         <main>
@@ -186,18 +108,7 @@ export const StatisticsPage = ({ title, description, footer }: IStatisticsPagePr
                             Filters
                         </DropdownToggle>
                         <DropdownMenu>
-                            <DropdownItem>
-                                All
-                            </DropdownItem>
-                            <DropdownItem>
-                                Metal
-                            </DropdownItem>
-                            <DropdownItem>
-                                Plastic
-                            </DropdownItem>
-                            <DropdownItem>
-                                Paper
-                            </DropdownItem>
+                            {useFilters(filters, setFilter)}
                         </DropdownMenu>
                     </Dropdown>
                     <div>
