8c9d6abc49f8b41d0678f4d3c36d640da00e3ee4
jendos2001 <jendos2001@yandex.ru>
2022-12-12 02:23:46 +0300

    feat(backend): add multiply filters in AvailableOrdersController and TransitController

--- backend/controllers/AvailableOrdersController.ts
+++ backend/controllers/AvailableOrdersController.ts
@@ -25,7 +25,7 @@ export default class AvailableOrdersController extends BaseController {
     @Post("/orders")
     public async orders(): Promise<Orders> {
         const {
-            filter, filterValue, page, perPage,
+            mainFilter, mainFilterValue, filters, page, perPage,
         } = this.req.body;
         let findDocs: Array<any> = [];
         const orders: Array<Order> = [];
@@ -39,17 +39,96 @@ export default class AvailableOrdersController extends BaseController {
             skip = (Number(page) - 1) * Number(perPage);
             limit = Number(perPage);
         }
-        if (filter === "Point of Departure") {
-            findDocs = await order.aggregate([{ $match: { "reception.address": { $regex: filterValue, $options: "i" }, status: "For export" } }]).sort({ date: 1 }).skip(skip).limit(limit);
-            const allFindDocs = await order.aggregate([{ $match: { "reception.address": { $regex: filterValue, $options: "i" }, status: "For export" } }]);
+
+        let startDate: Date;
+        let endDate: Date;
+        let startAmount: number;
+        let endAmount: number;
+        if (filters.date.from === "") {
+            if (filters.date.to === "") {
+                startDate = new Date(0);
+                endDate = new Date();
+            } else {
+                startDate = new Date(0);
+                endDate = new Date(filters.date.to);
+            }
+        } else if (filters.date.to === "") {
+            startDate = new Date(filters.date.from);
+            endDate = new Date();
+        } else {
+            startDate = new Date(filters.date.from);
+            endDate = new Date(filters.date.to);
+        }
+
+        if (filters.amount.from === "") {
+            if (filters.amount.to === "") {
+                startAmount = 0;
+                endAmount = Number.MAX_SAFE_INTEGER;
+            } else {
+                startAmount = 0;
+                endAmount = parseInt(filters.amount.to, 10);
+            }
+        } else if (filters.amount.to === "") {
+            startAmount = parseInt(filters.amount.from, 10);
+            endAmount = Number.MAX_SAFE_INTEGER;
+        } else {
+            startAmount = parseInt(filters.amount.from, 10);
+            endAmount = parseInt(filters.amount.to, 10);
+        }
+
+        if (mainFilter === "Point of Departure") {
+            findDocs = await order.aggregate([{
+                $match: {
+                    "reception.address": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]).sort({ date: 1 }).skip(skip).limit(limit);
+            const allFindDocs = await order.aggregate([{
+                $match: {
+                    "reception.address": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]);
             countOrders = allFindDocs.length;
-        } if (filter === "Type of waste") {
-            findDocs = await order.aggregate([{ $match: { "material.title": { $regex: filterValue, $options: "i" }, status: "For export" } }]).sort({ date: 1 }).skip(skip).limit(limit);
-            const allFindDocs = await order.aggregate([{ $match: { "material.title": { $regex: filterValue, $options: "i" }, status: "For export" } }]);
+        } if (mainFilter === "Type of waste") {
+            findDocs = await order.aggregate([{
+                $match: {
+                    "material.title": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]).sort({ date: 1 }).skip(skip).limit(limit);
+            const allFindDocs = await order.aggregate([{
+                $match: {
+                    "material.title": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]);
             countOrders = allFindDocs.length;
-        } if (filter === "Subtype") {
-            findDocs = await order.aggregate([{ $match: { "material.subtype": { $regex: filterValue, $options: "i" }, status: "For export" } }]).sort({ date: 1 }).skip(skip).limit(limit);
-            const allFindDocs = await order.aggregate([{ $match: { "material.subtype": { $regex: filterValue, $options: "i" }, status: "For export" } }]);
+        } if (mainFilter === "Subtype") {
+            findDocs = await order.aggregate([{
+                $match: {
+                    "material.subtype": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]).sort({ date: 1 }).skip(skip).limit(limit);
+            const allFindDocs = await order.aggregate([{
+                $match: {
+                    "material.subtype": { $regex: mainFilterValue, $options: "i" },
+                    status: "For export",
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                },
+            }]);
             countOrders = allFindDocs.length;
         }
         for (let i = 0; i < findDocs.length; i += 1) {
--- backend/controllers/TransitController.ts
+++ backend/controllers/TransitController.ts
@@ -22,7 +22,7 @@ export default class MainStorageController extends BaseController {
     @Post("/transit")
     public async transit(): Promise<Transits> {
         const {
-            filter, filterValue, page, perPage,
+            mainFilter, mainFilterValue, filters, page, perPage,
         } = this.req.body;
         let query: {};
         let skip: number;
@@ -36,12 +36,58 @@ export default class MainStorageController extends BaseController {
             skip = (Number(page) - 1) * Number(perPage);
             limit = Number(perPage);
         }
-        if (filter === "Reception") {
-            findDocs = await order.find({ "reception.address": { $regex: filterValue, $options: "i" }, status: "In delivery" }, {
+
+        let startDate: Date;
+        let endDate: Date;
+        let startAmount: number;
+        let endAmount: number;
+        const { type } = filters;
+        const { subType } = filters;
+        if (filters.date.from === "") {
+            if (filters.date.to === "") {
+                startDate = new Date(0);
+                endDate = new Date();
+            } else {
+                startDate = new Date(0);
+                endDate = new Date(filters.date.to);
+            }
+        } else if (filters.date.to === "") {
+            startDate = new Date(filters.date.from);
+            endDate = new Date();
+        } else {
+            startDate = new Date(filters.date.from);
+            endDate = new Date(filters.date.to);
+        }
+
+        if (filters.amount.from === "") {
+            if (filters.amount.to === "") {
+                startAmount = 0;
+                endAmount = Number.MAX_SAFE_INTEGER;
+            } else {
+                startAmount = 0;
+                endAmount = parseInt(filters.amount.to, 10);
+            }
+        } else if (filters.amount.to === "") {
+            startAmount = parseInt(filters.amount.from, 10);
+            endAmount = Number.MAX_SAFE_INTEGER;
+        } else {
+            startAmount = parseInt(filters.amount.from, 10);
+            endAmount = parseInt(filters.amount.to, 10);
+        }
+
+        if (mainFilter === "Reception") {
+            findDocs = await order.find({
+                "reception.address": { $regex: mainFilterValue, $options: "i" },
+                status: "In delivery",
+                "material.title": { $regex: type, options: "i" },
+                "material.subtype": { $regex: subType, options: "i" },
+                date: { $gte: startDate, $lte: endDate },
+                "material.count": { $gte: startAmount, $lte: endAmount },
+            }, {
                 users: 1, "reception.address": 1, "material.title": 1, "material.subtype": 1, "material.count": 1, history: 1, _id: 0,
             });
-        } if (filter === "Carrier") {
-            const userData: Array<string> = filterValue.split(" ");
+        } if (mainFilter === "Carrier") {
+            const userData: Array<string> = mainFilterValue.split(" ");
             if (userData.length === 1) {
                 query = {
                     role: "Driver",
@@ -61,7 +107,14 @@ export default class MainStorageController extends BaseController {
             }
             for (let i = 0; i < findOrders.length; i += 1) {
                 // eslint-disable-next-line no-await-in-loop
-                findDocs = findDocs.concat(await order.find({ _id: { $in: findOrders[i].orders }, status: "In delivery" }, {
+                findDocs = findDocs.concat(await order.find({
+                    _id: { $in: findOrders[i].orders },
+                    status: "In delivery",
+                    "material.title": { $regex: type, options: "i" },
+                    "material.subtype": { $regex: subType, options: "i" },
+                    date: { $gte: startDate, $lte: endDate },
+                    "material.count": { $gte: startAmount, $lte: endAmount },
+                }, {
                     users: 1, "reception.address": 1, "material.title": 1, "material.subtype": 1, "material.count": 1, history: 1, _id: 0,
                 }));
             }
